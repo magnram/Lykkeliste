1a2d53b0e8bc95bef2d1941bf2b58a804ae8003c
Magnus Ramm <magnus.ramm@gmail.com>
2024-10-29 15:08:39 +0100

    Initial Commit

---
+++ Listelykke/ListelykkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListelykkeApp.swift
+//  Listelykke
+//
+//  Created by Magnus Ramm on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListelykkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
